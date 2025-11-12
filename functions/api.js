03eb7ce8e93104c674cffcea2c8f20f6f2b5429c
Tom Hevesi <105016692+Froxy555@users.noreply.github.com>
2025-11-12 21:37:07 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -21,30 +21,62 @@ exports.handler = async (event, context) => {
     } else if (event.httpMethod === 'GET' && event.path.startsWith('/.netlify/functions/api/survey-results/')) {
       const surveyNumber = event.path.split('/').pop();
       const surveys = database.collection(`surveys${surveyNumber}`);
+      const pipeline = (surveyNumber === '5')
+        ? [
+            {
+              $group: {
+                _id: null,
+                bankiDual: { $push: '$bankiDual' },
+                bankiRobot: { $push: '$bankiRobot' },
+                byteBetyarok: { $push: '$byteBetyarok' },
+                talkReader: { $push: '$talkReader' },
+                telePoci: { $push: '$telePoci' },
+                pihenTECH: { $push: '$pihenTECH' }
+              }
+            },
+            {
+              $project: {
+                _id: 0,
+                bankiDual: 1,
+                bankiRobot: 1,
+                byteBetyarok: 1,
+                talkReader: 1,
+                telePoci: 1,
+                pihenTECH: 1
+              }
+            }
+          ]
+        : [
+            {
+              $group: {
+                _id: null,
+                satisfaction1: { $push: '$satisfaction1' },
+                satisfaction2: { $push: '$satisfaction2' },
+                satisfaction3: { $push: '$satisfaction3' },
+                favoriteTalk: { $push: '$favoriteTalk' }
+              }
+            },
+            {
+              $project: {
+                _id: 0,
+                satisfaction1: 1,
+                satisfaction2: 1,
+                satisfaction3: 1,
+                favoriteTalk: 1
+              }
+            }
+          ];
+
       const [aggregateResults, opinions] = await Promise.all([
-        surveys.aggregate([
-          {
-            $group: {
-              _id: null,
-              satisfaction1: { $push: '$satisfaction1' },
-              satisfaction2: { $push: '$satisfaction2' },
-              satisfaction3: { $push: '$satisfaction3' }
-            }
-          },
-          {
-            $project: {
-              _id: 0,
-              satisfaction1: 1,
-              satisfaction2: 1,
-              satisfaction3: 1
-            }
-          }
-        ]).toArray(),
+        surveys.aggregate(pipeline).toArray(),
         surveys.find({opinion: {$exists: true, $ne: ""}}, {projection: {_id: 0, opinion: 1}}).toArray()
       ]);
 
-      const results = aggregateResults[0] || { satisfaction1: [], satisfaction2: [], satisfaction3: [] };
-      results.opinions = opinions.map(doc => doc.opinion);
+      const results = aggregateResults[0] || (surveyNumber === '5'
+        ? { bankiDual: [], bankiRobot: [], byteBetyarok: [], talkReader: [], telePoci: [], pihenTECH: [] }
+        : { satisfaction1: [], satisfaction2: [], satisfaction3: [], favoriteTalk: [] }
+      );
+      results.opinions = (surveyNumber === '5') ? [] : opinions.map(doc => doc.opinion);
 
       return {
         statusCode: 200,
